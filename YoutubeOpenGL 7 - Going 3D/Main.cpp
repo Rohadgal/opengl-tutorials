9e1f0b64c516b7c89927163231f01275df6bff09
Rohadgal <gal.frias@gmail.com>
2024-02-27 22:38:20 -0600

    4 cubes drawn spinning on center axis

--- YoutubeOpenGL 7 - Going 3D/Main.cpp
+++ YoutubeOpenGL 7 - Going 3D/Main.cpp
@@ -2,6 +2,7 @@
 #include<filesystem>
 namespace fs = std::filesystem;
 //------------------------------
+#include <vector>
 
 #include<iostream>
 #include<glad/glad.h>
@@ -21,6 +22,200 @@ namespace fs = std::filesystem;
 const unsigned int width = 800;
 const unsigned int height = 800;
 
+
+struct cube_rotate {
+
+	GLfloat angle, x, y, z;
+
+};
+
+GLfloat angle;
+GLint rot_x, rot_y, crement, x_0, x_k, y_0, y_k, z_0, z_k, gap, gap_crement;
+cube_rotate cube_rotations[3][3][3];
+//vector<cube_rotate> cube_rotations[3][3][3];
+
+void apply_rotation(GLfloat angle) {
+
+	cube_rotate face[3][3];
+	int index;
+	cube_rotate rotation = cube_rotate();
+
+	// copy face to be rotated
+	// apply rotation to face
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 3; ++j) {
+
+			index = 2 - j % 3;
+
+			if (x_0 == x_k) {
+				rotation = { angle, 1.0, 0.0, 0.0 };
+				face[index][i] = cube_rotations[x_k][i][j];
+			}
+
+			if (y_0 == y_k) {
+				rotation = { angle, 0.0, 1.0, 0.0 };
+				face[index][i] = cube_rotations[j][y_k][i];
+			}
+
+			if (z_0 == z_k) {
+				rotation = { -1 * angle, 0.0, 0.0, 1.0 };
+				face[index][i] = cube_rotations[j][i][z_k];
+			}
+
+			face[index][i] = rotation;
+
+		}
+
+	// copy back rotated face
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 3; ++j) {
+
+			if (x_0 == x_k)
+				cube_rotations[x_k][i][j] = face[i][j];
+
+			if (y_0 == y_k)
+				cube_rotations[j][y_k][i] = face[i][j];
+
+			if (z_0 == z_k)
+				cube_rotations[j][i][z_k] = face[i][j];
+		}
+
+}
+
+// reset face selection parameters
+void reset_selected_face() {
+
+	x_0 = 0;
+	x_k = 2;
+	y_0 = 0;
+	y_k = 2;
+	z_0 = 0;
+	z_k = 2;
+
+}
+
+// keyboard function callback
+void keyboard_func(unsigned char key, int x, int y) {
+
+	switch (key) {
+
+	case '+':
+		gap += gap_crement;
+		break;
+
+	case '-':
+		gap -= gap_crement;
+		break;
+		// view rotation
+		// INcrement or DEcrement
+	case 'L': // right
+	case 'l':
+		rot_y = (rot_y - crement) % 360;
+		break;
+
+	case 'J': // left
+	case 'j':
+		rot_y = (rot_y + crement) % 360;
+		break;
+
+	case 'I': // down
+	case 'i':
+		rot_x = (rot_x + crement) % 360;
+		break;
+
+	case 'K': // up
+	case 'k':
+		rot_x = (rot_x - crement) % 360;
+		break;
+		// end of view rotation
+
+		// cube movements
+
+		// select cube face
+		// x-axis faces
+	case 'Q':
+	case 'q':
+		reset_selected_face();
+		x_0 = 0;
+		x_k = 0;
+		break;
+
+	case 'W':
+	case 'w':
+		reset_selected_face();
+		x_0 = 1;
+		x_k = 1;
+		break;
+
+	case 'E':
+	case 'e':
+		reset_selected_face();
+		x_0 = 2;
+		x_k = 2;
+		break;
+
+		// y-axis faces
+	case 'A':
+	case 'a':
+		reset_selected_face();
+		y_0 = 0;
+		y_k = 0;
+		break;
+
+	case 'S':
+	case 's':
+		reset_selected_face();
+		y_0 = 1;
+		y_k = 1;
+		break;
+
+	case 'D':
+	case 'd':
+		reset_selected_face();
+		y_0 = 2;
+		y_k = 2;
+		break;
+
+		// z-axis faces
+	case 'C':
+	case 'c':
+		reset_selected_face();
+		z_0 = 0;
+		z_k = 0;
+		break;
+
+	case 'X':
+	case 'x':
+		reset_selected_face();
+		z_0 = 1;
+		z_k = 1;
+		break;
+
+	case 'Z':
+	case 'z':
+		reset_selected_face();
+		z_0 = 2;
+		z_k = 2;
+		break;
+
+		// move selected face
+	case 'U': // counter-clockwise
+	case 'u':
+		apply_rotation(-90);
+		break;
+
+	case 'O': // clockwise
+	case 'o':
+		apply_rotation(90);
+		break;
+
+		// end of cube movements
+
+	default:
+		break;
+
+	}
+}
 
 // Vertices coordinates
 GLfloat vertices[] =
@@ -93,12 +288,8 @@ int main()
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
 
-
-
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
-
-
 
 	// Generates Vertex Array Object and binds it
 	VAO VAO1;
@@ -167,37 +358,46 @@ int main()
 		// Initializes matrices so they are not the null matrix
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 model2 = glm::mat4(1.0f);
+		glm::mat4 model3 = glm::mat4(1.0f);
+		glm::mat4 model4 = glm::mat4(1.0f);
+		glm::mat4 model5 = glm::mat4(1.0f);
+
+
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
 
 		// Assigns different transformations to each matrix
 		//model = glm::rotate(model, glm::radians(rotation), glm::vec3(1.0f, 1.0f, 0.0f));
-		model = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.0f));
-		model2 = glm::translate(view, glm::vec3(2.5f, 0.0f, 0.0f));
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		model2 = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+		model3 = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		model4 = glm::translate(model, glm::vec3(2.0f, 1.0f, 0.0f));
+		model5 = glm::translate(model, glm::vec3(2.0f, -1.0f, 0.0f));
 
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -25.0f));
 		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-
-		// Outputs the matrices into the Vertex Shader
-		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		//int modelLoc2 = glGetUniformLocation(shaderProgram.ID, "model2");
-
 
 		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
+		// Outputs the matrices into the Vertex Shader
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");  // ID
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		//j += 0.005f;
-
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
-
-		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model3));
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model4));
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model5));
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
 		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
 		glUniform1f(uniID, 0.5f);
@@ -214,8 +414,6 @@ int main()
 		glfwPollEvents();
 	}
 
-
-
 	// Delete all the objects we've created
 	VAO1.Delete();
 	VBO1.Delete();
@@ -229,7 +427,5 @@ int main()
 	return 0;
 }
 
-
 // Rendering Multiple Objects in OpenGL - The Cherno
-
 // https://stackoverflow.com/questions/9503866/how-do-i-display-2-or-more-objects-in-opengl-model-view-projection-matrices
