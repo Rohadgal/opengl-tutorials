998092c762d643bf91b6afa2bf9611222b0f82d1
a <>
2024-02-29 12:55:34 -0600

    loop throuhg cubes rendering in different pos

--- YoutubeOpenGL 7 - Going 3D/Main.cpp
+++ YoutubeOpenGL 7 - Going 3D/Main.cpp
@@ -5,6 +5,7 @@ namespace fs = std::filesystem;
 #include <vector>
 
 #include<iostream>
+
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
@@ -18,209 +19,212 @@ namespace fs = std::filesystem;
 #include"VBO.h"
 #include"EBO.h"
 
+using namespace std;
 
 const unsigned int width = 800;
 const unsigned int height = 800;
-
-
-struct cube_rotate {
-
-	GLfloat angle, x, y, z;
-
-};
-
-GLfloat angle;
-GLint rot_x, rot_y, crement, x_0, x_k, y_0, y_k, z_0, z_k, gap, gap_crement;
-cube_rotate cube_rotations[3][3][3];
-//vector<cube_rotate> cube_rotations[3][3][3];
-
-void apply_rotation(GLfloat angle) {
-
-	cube_rotate face[3][3];
-	int index;
-	cube_rotate rotation = cube_rotate();
-
-	// copy face to be rotated
-	// apply rotation to face
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j) {
-
-			index = 2 - j % 3;
-
-			if (x_0 == x_k) {
-				rotation = { angle, 1.0, 0.0, 0.0 };
-				face[index][i] = cube_rotations[x_k][i][j];
-			}
-
-			if (y_0 == y_k) {
-				rotation = { angle, 0.0, 1.0, 0.0 };
-				face[index][i] = cube_rotations[j][y_k][i];
-			}
-
-			if (z_0 == z_k) {
-				rotation = { -1 * angle, 0.0, 0.0, 1.0 };
-				face[index][i] = cube_rotations[j][i][z_k];
-			}
-
-			face[index][i] = rotation;
-
-		}
-
-	// copy back rotated face
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j) {
-
-			if (x_0 == x_k)
-				cube_rotations[x_k][i][j] = face[i][j];
-
-			if (y_0 == y_k)
-				cube_rotations[j][y_k][i] = face[i][j];
-
-			if (z_0 == z_k)
-				cube_rotations[j][i][z_k] = face[i][j];
-		}
-
-}
-
-// reset face selection parameters
-void reset_selected_face() {
-
-	x_0 = 0;
-	x_k = 2;
-	y_0 = 0;
-	y_k = 2;
-	z_0 = 0;
-	z_k = 2;
-
-}
-
-// keyboard function callback
-void keyboard_func(unsigned char key, int x, int y) {
-
-	switch (key) {
-
-	case '+':
-		gap += gap_crement;
-		break;
-
-	case '-':
-		gap -= gap_crement;
-		break;
-		// view rotation
-		// INcrement or DEcrement
-	case 'L': // right
-	case 'l':
-		rot_y = (rot_y - crement) % 360;
-		break;
-
-	case 'J': // left
-	case 'j':
-		rot_y = (rot_y + crement) % 360;
-		break;
-
-	case 'I': // down
-	case 'i':
-		rot_x = (rot_x + crement) % 360;
-		break;
-
-	case 'K': // up
-	case 'k':
-		rot_x = (rot_x - crement) % 360;
-		break;
-		// end of view rotation
-
-		// cube movements
-
-		// select cube face
-		// x-axis faces
-	case 'Q':
-	case 'q':
-		reset_selected_face();
-		x_0 = 0;
-		x_k = 0;
-		break;
-
-	case 'W':
-	case 'w':
-		reset_selected_face();
-		x_0 = 1;
-		x_k = 1;
-		break;
-
-	case 'E':
-	case 'e':
-		reset_selected_face();
-		x_0 = 2;
-		x_k = 2;
-		break;
-
-		// y-axis faces
-	case 'A':
-	case 'a':
-		reset_selected_face();
-		y_0 = 0;
-		y_k = 0;
-		break;
-
-	case 'S':
-	case 's':
-		reset_selected_face();
-		y_0 = 1;
-		y_k = 1;
-		break;
-
-	case 'D':
-	case 'd':
-		reset_selected_face();
-		y_0 = 2;
-		y_k = 2;
-		break;
-
-		// z-axis faces
-	case 'C':
-	case 'c':
-		reset_selected_face();
-		z_0 = 0;
-		z_k = 0;
-		break;
-
-	case 'X':
-	case 'x':
-		reset_selected_face();
-		z_0 = 1;
-		z_k = 1;
-		break;
-
-	case 'Z':
-	case 'z':
-		reset_selected_face();
-		z_0 = 2;
-		z_k = 2;
-		break;
-
-		// move selected face
-	case 'U': // counter-clockwise
-	case 'u':
-		apply_rotation(-90);
-		break;
-
-	case 'O': // clockwise
-	case 'o':
-		apply_rotation(90);
-		break;
-
-		// end of cube movements
-
-	default:
-		break;
-
-	}
-}
+//
+//
+//struct cube_rotate {
+//
+//	GLfloat angle, x, y, z;
+//
+//};
+//
+//GLfloat angle;
+//GLint rot_x, rot_y, crement, x_0, x_k, y_0, y_k, z_0, z_k, gap, gap_crement;
+//cube_rotate cube_rotations[3][3][3];
+////vector<cube_rotate> cube_rotations[3][3][3];
+//
+//void apply_rotation(GLfloat angle) {
+//
+//	cube_rotate face[3][3];
+//	int index;
+//	cube_rotate rotation = cube_rotate();
+//
+//	// copy face to be rotated
+//	// apply rotation to face
+//	for (int i = 0; i < 3; ++i)
+//		for (int j = 0; j < 3; ++j) {
+//
+//			index = 2 - j % 3;
+//
+//			if (x_0 == x_k) {
+//				rotation = { angle, 1.0, 0.0, 0.0 };
+//				face[index][i] = cube_rotations[x_k][i][j];
+//			}
+//
+//			if (y_0 == y_k) {
+//				rotation = { angle, 0.0, 1.0, 0.0 };
+//				face[index][i] = cube_rotations[j][y_k][i];
+//			}
+//
+//			if (z_0 == z_k) {
+//				rotation = { -1 * angle, 0.0, 0.0, 1.0 };
+//				face[index][i] = cube_rotations[j][i][z_k];
+//			}
+//
+//			face[index][i] = rotation;
+//
+//		}
+//
+//	// copy back rotated face
+//	for (int i = 0; i < 3; ++i)
+//		for (int j = 0; j < 3; ++j) {
+//
+//			if (x_0 == x_k)
+//				cube_rotations[x_k][i][j] = face[i][j];
+//
+//			if (y_0 == y_k)
+//				cube_rotations[j][y_k][i] = face[i][j];
+//
+//			if (z_0 == z_k)
+//				cube_rotations[j][i][z_k] = face[i][j];
+//		}
+//
+//}
+//
+//// reset face selection parameters
+//void reset_selected_face() {
+//
+//	x_0 = 0;
+//	x_k = 2;
+//	y_0 = 0;
+//	y_k = 2;
+//	z_0 = 0;
+//	z_k = 2;
+//
+//}
+//
+//// keyboard function callback
+//void keyboard_func(unsigned char key, int x, int y) {
+//
+//	switch (key) {
+//
+//	case '+':
+//		gap += gap_crement;
+//		break;
+//
+//	case '-':
+//		gap -= gap_crement;
+//		break;
+//		// view rotation
+//		// INcrement or DEcrement
+//	case 'L': // right
+//	case 'l':
+//		rot_y = (rot_y - crement) % 360;
+//		break;
+//
+//	case 'J': // left
+//	case 'j':
+//		rot_y = (rot_y + crement) % 360;
+//		break;
+//
+//	case 'I': // down
+//	case 'i':
+//		rot_x = (rot_x + crement) % 360;
+//		break;
+//
+//	case 'K': // up
+//	case 'k':
+//		rot_x = (rot_x - crement) % 360;
+//		break;
+//		// end of view rotation
+//
+//		// cube movements
+//
+//		// select cube face
+//		// x-axis faces
+//	case 'Q':
+//	case 'q':
+//		reset_selected_face();
+//		x_0 = 0;
+//		x_k = 0;
+//		break;
+//
+//	case 'W':
+//	case 'w':
+//		reset_selected_face();
+//		x_0 = 1;
+//		x_k = 1;
+//		break;
+//
+//	case 'E':
+//	case 'e':
+//		reset_selected_face();
+//		x_0 = 2;
+//		x_k = 2;
+//		break;
+//
+//		// y-axis faces
+//	case 'A':
+//	case 'a':
+//		reset_selected_face();
+//		y_0 = 0;
+//		y_k = 0;
+//		break;
+//
+//	case 'S':
+//	case 's':
+//		reset_selected_face();
+//		y_0 = 1;
+//		y_k = 1;
+//		break;
+//
+//	case 'D':
+//	case 'd':
+//		reset_selected_face();
+//		y_0 = 2;
+//		y_k = 2;
+//		break;
+//
+//		// z-axis faces
+//	case 'C':
+//	case 'c':
+//		reset_selected_face();
+//		z_0 = 0;
+//		z_k = 0;
+//		break;
+//
+//	case 'X':
+//	case 'x':
+//		reset_selected_face();
+//		z_0 = 1;
+//		z_k = 1;
+//		break;
+//
+//	case 'Z':
+//	case 'z':
+//		reset_selected_face();
+//		z_0 = 2;
+//		z_k = 2;
+//		break;
+//
+//		// move selected face
+//	case 'U': // counter-clockwise
+//	case 'u':
+//		apply_rotation(-90);
+//		break;
+//
+//	case 'O': // clockwise
+//	case 'o':
+//		apply_rotation(90);
+//		break;
+//
+//		// end of cube movements
+//
+//	default:
+//		break;
+//
+//	}
+//}
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //     COORDINATES     /        COLORS      /   TexCoord  //
-	-0.5f, -0.5f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.25f,  //1  // *4
+{ 
+#pragma region FirstCube
+	//     COORDINATES     /        COLORS      /   TexCoord  //
+	- 0.5f, -0.5f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.25f,  //1  // *4
 	-0.5f, -0.5f, -0.5f,     0.83f, 0.70f, 0.44f,	0.75f, 0.25f, // 3
 	 0.5f, -0.5f, -0.5f,     0.83f, 0.70f, 0.44f,	0.5f, 0.25f, //2
 	 0.5f, -0.5f,  0.5f,     0.83f, 0.70f, 0.44f,	0.25f, 0.25f,  // 1  //2
@@ -236,6 +240,9 @@ GLfloat vertices[] =
 	-0.5f, 0.5f, 0.5f,     0.92f, 0.86f, 0.76f,		0.75f, 0.75f,  // 4  *6
 	-0.5f, -0.5f,  0.5f,     0.83f, 0.70f, 0.44f,	0.75f, 0.0f,  //0  *5
 	 0.5f, -0.5f,  0.5f,     0.83f, 0.70f, 0.44f,	0.5f, 0.0f,  // 3  *5
+#pragma endregion
+
+	
 };
 
 // Indices for vertices order
@@ -355,32 +362,9 @@ int main()
 			prevTime = crntTime;
 		}
 
-		// Initializes matrices so they are not the null matrix
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 model2 = glm::mat4(1.0f);
-		glm::mat4 model3 = glm::mat4(1.0f);
-		glm::mat4 model4 = glm::mat4(1.0f);
-		glm::mat4 model5 = glm::mat4(1.0f);
-		glm::mat4 model6 = glm::mat4(1.0f);
-		glm::mat4 model7 = glm::mat4(1.0f);
-		glm::mat4 model8 = glm::mat4(1.0f);
-
 
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
-
-		// Assigns different transformations to each matrix
-		//model = glm::rotate(model, glm::radians(rotation), glm::vec3(1.0f, 1.0f, 0.0f));
-
-		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::translate(model, glm::vec3(-1.0f, -1.0f, -1.0f));
-		model2 = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
-		model3 = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-		model4 = glm::translate(model, glm::vec3(0.0f, 1.0f, 0.0f));
-		model5 = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f));
-		model6 = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
-		model7 = glm::translate(model, glm::vec3(0.0f, 0.0f, 2.0f));
-		model8 = glm::translate(model, glm::vec3(1.0f, 1.0f, 1.0f));
 
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -25.0f));
 		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
@@ -389,27 +373,77 @@ int main()
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-		// Outputs the matrices into the Vertex Shader
 		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");  // ID
+		// Initializes matrices so they are not the null matrix
 
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model3));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model4));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model5));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model6));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model7));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model8));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		glm::mat4 model[3] = {
+			glm::mat4(1.0f),
+			glm::mat4(1.0f),
+			glm::mat4(1.0f),
+		};
+
+		glm::vec3 pos[3] = {
+			glm::vec3(-1.0f, -1.0f,-1.0f),
+			glm::vec3(0.0f, -1.0f, -1.0f),
+			glm::vec3(1.0f, -1.0f, -1.0f),
+		};
+
+		glm::mat4 model4 = glm::mat4(1.0f);
+		glm::mat4 model5 = glm::mat4(1.0f);
+		glm::mat4 model6 = glm::mat4(1.0f);
+		glm::mat4 model7 = glm::mat4(1.0f);
+		glm::mat4 model8 = glm::mat4(1.0f);
+
+		
+
+		for (int i = 0; i < 3; i++) {
+			model[i] = glm::translate(model[i], pos[i]);
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model[i]));
+			glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+			//model[i] = glm::translate(model[i], -pos[i]);
+			
+			cout<< pos[1].x << endl;
+		}
+
+		// Assigns different transformations to each matrix
+		//model = glm::rotate(model, glm::radians(rotation), glm::vec3(1.0f, 1.0f, 0.0f));
+
+		/*model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(-1.0f, -1.0f, -1.0f));
+		model2 = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+		model3 = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		model4 = glm::translate(model, glm::vec3(0.0f, 1.0f, 0.0f));
+		model5 = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f));
+		model6 = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
+		model7 = glm::translate(model, glm::vec3(0.0f, 0.0f, 2.0f));
+		model8 = glm::translate(model, glm::vec3(1.0f, 1.0f, 1.0f));*/
+
+		
+
+		//// Outputs the matrices into the Vertex Shader
+		//int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");  // ID
+
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		//// Draw primitives, number of indices, datatype of indices, index of indices
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		 
+		
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model3));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model4));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model5));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model6));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model7));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model8));
+		//glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		// 
+		// 
 		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
 		glUniform1f(uniID, 0.5f);
 		
